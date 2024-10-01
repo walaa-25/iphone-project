5aea19aa465f5c837311ce5501667ae95f4cca29
walaa-25 <gydrudt.65734bcvnbr@gmail.com>
2024-10-01 15:23:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let img=document.getElementById('img');
+let container=document.querySelector('.container');
+function phone(src){
+    img.src=src;
+}
+function color(color){
+    container.style.background=color
+
+}
